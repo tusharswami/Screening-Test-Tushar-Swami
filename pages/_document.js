918657038b0b12afd4840fdc926b32811b28d2cf
tusharswami <tusharswami01@gmail.com>
2020-09-18 00:14:18 +0530

    GitHub pages_v1

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="shortcut icon" href="../static/images/favicon.ico" />
+          <link rel="shortcut icon" href="https://tusharswami.github.io/Screening-Test-Tushar-Swami/static/images/favicon.ico" />
           <title>Tushar Swami: Test Solution build with NextJS</title>
           <meta name="description" content="This app has been build with regards as a Screening test for Frontend Developer. This app has been solely build by Tushar Swami using NextJS"/>
         </Head>
